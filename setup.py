088d28e4ff7af65cfb1a3748ca47bd2d9a8a7a41
Darwin Monroy <contact@darwinmonroy.com>
2018-02-05 00:31:48 -0600

    remove encoding from open

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(here, 'README.md')) as f:
     long_description = f.read()
 
 setup(

bff4745365803117c3cdf5fac01d4a119c117ba6
Darwin Monroy <contact@darwinmonroy.com>
2018-02-05 00:26:01 -0600

    added classifiers

--- setup.py
+++ setup.py
@@ -15,6 +15,17 @@ setup(
         ]
     },
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Framework :: Pytest",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Build Tools",
     ],
 )
